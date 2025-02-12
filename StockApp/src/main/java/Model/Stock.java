19c2788c1a084d4903b318da0f93de62871eabde
lahcen404 <lahcen.maskour2003@gmail.com>
2025-02-12 11:45:18 +0100

    create geters and seters with class

--- StockApp/src/main/java/Model/Stock.java
+++ StockApp/src/main/java/Model/Stock.java
@@ -2,4 +2,54 @@ package Model;
 
 public class Stock {
 
+	private int id;
+	private String quantity;
+	private String PrixUnite;
+	private String Category;
+	private String Description;
+	
+	
+	
+	public Stock(int id, String quantity, String prixUnite, String category, String description) {
+		super();
+		this.id = id;
+		this.quantity = quantity;
+		this.PrixUnite = prixUnite;
+		this.Category = category;
+		this.Description = description;
+	}
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+	public String getPrixUnite() {
+		return PrixUnite;
+	}
+	public void setPrixUnite(String prixUnite) {
+		PrixUnite = prixUnite;
+	}
+	public String getCategory() {
+		return Category;
+	}
+	public void setCategory(String category) {
+		Category = category;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	
 }
